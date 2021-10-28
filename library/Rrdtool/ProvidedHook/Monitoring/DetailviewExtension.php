e5641ede22905cedc72f74f2ea9ecc8a1987309c
Virsacer <virsacer@online.de>
2021-10-28 17:27:10 +0200

    Add labels to thumbnails

--- library/Rrdtool/ProvidedHook/Monitoring/DetailviewExtension.php
+++ library/Rrdtool/ProvidedHook/Monitoring/DetailviewExtension.php
@@ -20,11 +20,11 @@ class DetailviewExtension extends DetailviewExtensionHook {
 			ob_start();
 			?>
 			<h2><?php echo $view->translate("Graphs"); ?></h2>
-			<a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=year" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=year" alt=""/></a>
-			<a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=month" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=month" alt=""/></a>
-			<a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=week" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=week" alt=""/></a>
-			<a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=day" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=day" alt=""/></a>
-			<a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=hours" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=hours" alt=""/></a>
+			<figure><a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=year" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=year" alt=""/></a><figcaption>1 <?php echo $view->translate("Year"); ?></figcaption></figure>
+			<figure><a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=month" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=month" alt=""/></a><figcaption>1 <?php echo $view->translate("Month"); ?></figcaption></figure>
+			<figure><a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=week" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=week" alt=""/></a><figcaption>1 <?php echo $view->translate("Week"); ?></figcaption></figure>
+			<figure><a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=day" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=day" alt=""/></a><figcaption>1 <?php echo $view->translate("Day"); ?></figcaption></figure>
+			<figure><a href="<?php echo $view->href("rrdtool/graph/view", $params); ?>&amp;range=hours" data-base-target="_next"><img src="<?php echo $view->href("rrdtool/graph?thumb", $params); ?>&amp;range=hours" alt=""/></a><figcaption>4 <?php echo $view->translate("Hours"); ?></figcaption></figure>
 			<?php
 			return ob_get_clean();
 		}
